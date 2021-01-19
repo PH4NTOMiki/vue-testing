c1d91bc9544d1866010d50307a63c84b2b718989
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-01-19 16:29:21 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+declare global { interface Window { snapSaveState: Function } }
+
+window.snapSaveState = () => {
+    document.querySelector("#app")?.setAttribute("data-server-rendered", "true");
+};
